4cb70d4fbaf6714811254f595c8d433ea0acdb4c
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-12-13 14:51:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+distance_to_object=0
+x=distance_to_object
+def on_forever(function):
+    global distance_to_object
+    distance_to_object= (sonar.ping(digitalpin.p1, digitalpin.p2, pingunit.centimeters)
+  display(x)
+(on_forever)
+
+distance_to_object=0
+x=distance_to_object
+def on_forever(function):
+    global distance_to_object
+    x=(sonar.ping pin=p1:,pin.p2,unit.centimeters)):
+print (x)
+(on_forever)
