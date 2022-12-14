486acec098ab61fbae1d1ab8b7e11e3bd414954c
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-12-14 15:00:05 +0000

    function of code

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-distance_to_object=0
-x=distance_to_object
-def on_forever(function):
-    global distance_to_object
-    distance_to_object= (sonar.ping(digitalpin.p1, digitalpin.p2, pingunit.centimeters)
-  display(x)
-(on_forever)
+class HCSR04:
+    # this class abstracts out the functionality of the HC-SR04 and
+    #   returns distance in mm
+    # Trig: pin 1
+    # Echo: pin 2
+    def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
+        self.trigger_pin = tpin
+        self.echo_pin = epin
+        self.sclk_pin = spin
 
-distance_to_object=0
-x=distance_to_object
-def on_forever(function):
-    global distance_to_object
-    x=(sonar.ping pin=p1:,pin.p2,unit.centimeters)):
-print (x)
-(on_forever)
+    def distance_mm(self):
+        spi.init(baudrate=125000, sclk=self.sclk_pin,
+                 mosi=self.trigger_pin, miso=self.echo_pin)
+        pre = 0
+        post = 0
+        k = -1
+        length = 500
+        resp = bytearray(length)
+        resp[0] = 0xFF
+        spi.write_readinto(resp, resp)
+        # find first non zero value
+        try:
+            i, value = next((ind, v) for ind, v in enumerate(resp) if v)
+        except StopIteration:
+            i = -1
+        if i > 0:
+            pre = bin(value).count("1")
+            # find first non full high value afterwards
+            try:
+                k, value = next((ind, v)
+                                for ind, v in enumerate(resp[i:length - 2]) if resp[i + ind + 1] == 0)
+                post = bin(value).count("1") if k else 0
+                k = k + i
+            except StopIteration:
+                i = -1
+        dist= -1 if i < 0 else round(((pre + (k - i) * 8. + post) * 8 * 0.172) / 2)
+        return dist
+
+sonar= HCSR04()
+display.show(image.HEART)
+
+while True: 
+    display.show(sonar.distance_cm()/10)
+    sleep (500)
